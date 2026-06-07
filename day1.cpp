a1d64c166bf461d02403929b304c96d5c9449b8b
JAYANTI2006 <anjalijayanti2003@gmail.com>
2026-06-07 00:36:28 -0700

    day1.cpp

--- day1.cpp
+++ day1.cpp
@@ -4,53 +4,53 @@ using namespace std;
 //wap the sum of the digits of given numbers
 
 int main(){
-    // int num;
-    // int sum =0;
-    // cout << "enter a number \n";
-    // cin >> num;
+    int num;
+    int sum =0;
+    cout << "enter a number \n";
+    cin >> num;
     
-    // while(num > 0){
-    //     int digit = num %10;
-    //     sum += digit;
-    //     num /=10;
-    // }
-    // cout << "the sum is =  " << sum;   
-    // return 0;
+    while(num > 0){
+        int digit = num %10;
+        sum += digit;
+        num /=10;
+    }
+    cout << "the sum is =  " << sum;   
+    return 0;
     
-// }
+}
 
 
 //wap to reverse a program.
 
 
-//    int num;
-//    int rev =0;
-//    cout << "enter a number \n";
-//    cin >> num;
+   int num;
+   int rev =0;
+   cout << "enter a number \n";
+   cin >> num;
 
-//    while(num>0){
-//     int digit = num%10;
-//     rev = rev *10 +digit;
-//     num /=10;
-//    }
+   while(num>0){
+    int digit = num%10;
+    rev = rev *10 +digit;
+    num /=10;
+   }
 
-//    cout << " the reveresed no is " << rev;
+   cout << " the reveresed no is " << rev;
 
 
 // find the multilication of digits of a number
 
-//    int num;
-//    int mult =1;
-//    cout << "enter a number \n";
-//    cin >> num;
+   int num;
+   int mult =1;
+   cout << "enter a number \n";
+   cin >> num;
 
-//    while(num>0){
-//     int digit = num%10;
-//     mult = mult * digit;
-//     num /=10;
-//    }
+   while(num>0){
+    int digit = num%10;
+    mult = mult * digit;
+    num /=10;
+   }
 
-//    cout << " the multiplictaion of digits are =  " << mult;
+   cout << " the multiplictaion of digits are =  " << mult;
    return 0;
 }
 
