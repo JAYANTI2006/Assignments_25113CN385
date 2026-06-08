97142492ec819331e250bb705165363a57ecf11b
JAYANTI2006 <anjalijayanti2003@gmail.com>
2026-06-08 03:31:27 -0700

    Update day5.cpp

--- day5.cpp
+++ day5.cpp
@@ -1,1 +1,38 @@
 
+
+
+ int num;
+   cout << " enter a number";
+   cin >> num;
+  
+   
+   int n = num;
+   int sum= 0 ;
+   
+    if(n == 0){
+       sum = 1;
+   }
+   
+   while(n > 0){
+       
+    int  d = n % 10;
+    
+    int fact = 1;
+    while(d>1){
+        fact *= d;
+        d--;
+    }
+    sum +=fact;
+    n /= 10;
+    
+   }
+   if( sum == num){
+    cout << " strong";
+   }
+   else{
+    cout << " weak";
+   }
+   return 0;
+} 
+
+
