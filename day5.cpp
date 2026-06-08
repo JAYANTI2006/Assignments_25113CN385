f9c1bdfdd7a6867945590c36bff7459c5730ee01
JAYANTI2006 <anjalijayanti2003@gmail.com>
2026-06-08 04:12:37 -0700

    Update day5.cpp

--- day5.cpp
+++ day5.cpp
@@ -1,7 +1,31 @@
+#include <iostream>
+using namespace std;
 
+int main() {
 
+ //Q1. PERFECT NUMBER
+   int num;
+   cout << " enter a number =";
+   cin >> num;
+   int sum = 0;
+   for(int i = 1; i< num ; i++){
+       if(num % i == 0){
+           sum += i;
+       }
+   }
+   if(sum == num){
+       cout << "perfect number";
+   }
+   else{
+       cout << " not perfect";
+   }
+return 0;
+} 
+ 
 
- int num;
+ //Q2.  strong number
+
+   int num;
    cout << " enter a number";
    cin >> num;
   
@@ -34,5 +58,58 @@
    }
    return 0;
 } 
+
+
+// Q3. FACTORS OF A NUMBER
+
+   int num;
+   cout << " enter a number =";
+   cin >> num;
+
+   for(int i = 1; i< num ; i++){
+       if(num % i == 0){
+           cout << "factor = " << i << endl;
+       }
+   }
+  
+return 0;
+} 
+
+// Q4. LARGEST PRIME FACTOR
+
+    int num;
+    cout << "Enter a number = ";
+    cin >> num;
+
+    int max = -1;
+
+    for(int i = 1; i <= num; i++) {
+
+        if(num % i == 0) {
+
+            int isprime = 1;
+
+            if(i == 1) {
+                continue;
+            }
+
+            for(int j = 2; j < i; j++) {
+                if(i % j == 0) {
+                    isprime = 0;
+                    break;
+                }
+            }
+
+            if(isprime) {
+                max = i;
+            }
+        }
+    }
+
+    cout << "Greatest prime factor = " << max;
+
+    return 0;
+}
+
 
 
