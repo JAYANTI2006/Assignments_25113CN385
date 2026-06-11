a5f9d29d3d5bf12866cc68739b8c96469852034f
JAYANTI2006 <anjalijayanti2003@gmail.com>
2026-06-11 06:13:44 -0700

    Update day15.cpp

--- day15.cpp
+++ day15.cpp
@@ -4,6 +4,28 @@
 using namespace std;
 
 int main() {
+
+ //left shift
+    vector<int> v = {1, 2, 3, 4, 5, 6};
+    
+    unsigned int space; 
+    cout << " space = ";
+    cin >> space;
+ 
+    space = space % v.size(); 
+   
+    for(int k = 0; k < space ; k++){
+        int start = v[0];
+        for(int i = 0 ; i < v.size() - 1 ; i++){
+            v[i] = v[i+1];
+        }
+        v[v.size() - 1] = start;
+    }
+
+    for(int val : v) cout << val << " ";
+    return 0;
+}
+
   // right shift
     vector<int> v = {1,2,3,4,5,6};
     int  end;
