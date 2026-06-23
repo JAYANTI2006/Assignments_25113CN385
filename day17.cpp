eba6e3e149a4719124e1793f1d1e9b0a199779a6
JAYANTI2006 <anjalijayanti2003@gmail.com>
2026-06-23 09:22:28 -0700

    Update day17.cpp

--- day17.cpp
+++ day17.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <cstdlib>
+#include <unordered _set>
 using namespace std;
+
 
 
 int main()
 {
-  //MERGING TO TWO ARRAYS
+  //MERGING OF TWO ARRAYS
     int n;
     cout << "enter the size of array one =";
     cin >> n;
@@ -82,3 +84,90 @@ int main()
     
     return 0;
 }    
+
+
+//UNION OF ARRAYS
+
+#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+
+int main() {
+    int arr[] = {1, 3, 4, 5, 5, 1, 2, 4, 4};
+    int arr1[] = {1, 2, 2, 23, 7, 7, 4, 5};
+    unordered_set<int> ans;
+
+    int len1 = sizeof(arr) / sizeof(arr[0]);
+    int len2 = sizeof(arr1) / sizeof(arr1[0]);
+
+    for (int i = 0; i < len1; i++) {
+        ans.insert(arr[i]);
+    }
+
+    for (int i = 0; i < len2; i++) {
+        ans.insert(arr1[i]);
+    }
+
+    for (int element : ans) {
+        cout << element << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
+
+
+//INTERSECTION OF TWO ARRAYS
+
+    int arr[] = {1, 3, 4, 5, 5, 1, 2, 4, 4};
+    int arr1[] = {1, 2, 2, 23, 7, 7, 4, 5};
+    unordered_set<int> ans;
+
+    int len1 = sizeof(arr) / sizeof(arr[0]);
+    int len2 = sizeof(arr1) / sizeof(arr1[0]);
+    int small_len;
+    int larg_len;
+
+    int *dummy_arr;
+    int *dummy1_arr;
+
+
+    if (len1 == len2) {
+        small_len = len1;
+        larg_len = len2;
+
+        dummy_arr = arr;
+        dummy1_arr = arr1;
+
+    } 
+    else {
+        small_len = (len1 > len2 ? len2 : len1);
+        larg_len = (len1 > len2 ? len1 : len2);
+
+        dummy_arr = (len1 < len2 ? arr : arr1);
+        dummy1_arr = (len1 < len2 ? arr1 : arr);
+
+    }    
+
+    for (int i = 0; i < small_len; i++) {
+        for (int j = 0; j < larg_len; j++) {
+            if (dummy_arr[i] == dummy1_arr[j]) {
+                    ans.insert(dummy_arr[i]);
+                    break;
+            }
+        }
+    }
+    
+
+    for (int element : ans) {
+        cout << element << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
+
+
