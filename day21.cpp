88793cffef8eef03f441108aaf32125f3bd9feea
JAYANTI2006 <anjalijayanti2003@gmail.com>
2026-06-25 07:06:19 -0700

    Update day21.cpp

--- day21.cpp
+++ day21.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+using namespace std;
+int main (){
+  // LENGTH OF A STRING
+    string str = "hello";
+    int count = 0;
+    int i = 0;
+    while(str[i] != '\0'){
+        count++;
+        i++;
+    }
+    cout << " count of letters  = " << count;
+    return 0;
+} 
 
