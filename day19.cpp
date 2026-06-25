5cfd45d31e3458d8c2b0a5b2ca69f3373496512d
JAYANTI2006 <anjalijayanti2003@gmail.com>
2026-06-25 05:52:55 -0700

    Update day19.cpp

--- day19.cpp
+++ day19.cpp
@@ -40,3 +40,43 @@ int main(){
     
     return 0;
 } 
+
+// SUBTRACTION OF MATRICES
+
+    int arr[34][56] = {{1, 2, 3}, {4 ,5,6 },{7,43,56}};
+    int arr1[30][30] = { {8, 9 , 10}, {11 , 12 ,13}, {14,34, 56}};
+
+    int ans[45][45];
+    
+    int order_arr = 3;
+    int order_arr1 = 3; 
+
+    if(order_arr == order_arr1){
+       
+        for(int i = 0 ; i < order_arr ; i++){
+            for(int j = 0 ; j< order_arr ; j++){
+
+                int sub;
+                sub = arr[i][j] - arr1[i][j];
+                ans[i][j] = sub;
+            }
+        }
+
+    }
+    else{
+        cout << " subtraction  not possible";
+    }
+
+    cout << "["; 
+
+    for(int i = 0 ; i < order_arr ; i++){
+        for(int j = 0 ; j < order_arr ; j++){
+
+            cout <<  ans[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << "]";
+
+    return 0;
+} 
