78cf52baac6c3f1f94ee48a70aba54ef69e5b6f9
JAYANTI2006 <anjalijayanti2003@gmail.com>
2026-06-25 04:00:58 -0700

    Update day18.cpp

--- day18.cpp
+++ day18.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main{
 
   // BUBBLE SORTING 
-  int arr[] = { 5, 6 , 7 , 9 ,3 ,2 ,1};
+    int arr[] = { 5, 6 , 7 , 9 ,3 ,2 ,1};
     int len = sizeof(arr)/sizeof(arr[0]);
     for(int pass = 0 ; pass < len - 1; pass++){
         for(int sort = 0 ; sort < len - pass - 1 ; sort++){
@@ -23,4 +23,30 @@ int main{
         cout << arr[i] << " ";
     }
     return 0;
-}    
+} 
+
+//SELECTION SORT
+
+int arr[] = { 5, 6 , 7 , 9 ,3 ,2 ,1};
+int len = sizeof(arr)/sizeof(arr[0]);
+
+for(int i = 0; i < len ; i++){
+
+  int min_idx = i;
+
+  for(int j = i + 1 ; j < len ; j++){
+    if(arr[min_idx] > arr[j]){
+      min_idx = j;
+    
+    }  
+    int temp = arr[min_idx];
+    arr[min_idx] = arr[j];
+     arr[j] = temp;
+  
+    }
+  }  
+  for(int i = 0 ; i < len ; i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+} 
