ff6880e2e2d605fef5f438c78a180f22a11215b4
JAYANTI2006 <anjalijayanti2003@gmail.com>
2026-06-30 06:02:28 -0700

    Update day28.cpp

--- day28.cpp
+++ day28.cpp
@@ -1,1 +1,90 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+
+    // Q109. Create Library Management System
+
+    int bookId;
+    string bookName;
+    string author;
+
+    cout << "Enter Book ID: ";
+    cin >> bookId;
+
+    cout << "Enter Book Name: ";
+    cin >> bookName;
+
+    cout << "Enter Author Name: ";
+    cin >> author;
+
+    cout << "\nBook Details\n";
+    cout << "Book ID: " << bookId << endl;
+    cout << "Book Name: " << bookName << endl;
+    cout << "Author: " << author << endl;
+
+    return 0;
+}
+
+
+    // Q110. Create Bank Account System
+
+    string name;
+    int accountNo;
+    double balance;
+
+    cout << "Enter Account Holder Name: ";
+    cin >> name;
+
+    cout << "Enter Account Number: ";
+    cin >> accountNo;
+
+    cout << "Enter Balance: ";
+    cin >> balance;
+
+    cout << "\nAccount Details\n";
+    cout << "Name: " << name << endl;
+    cout << "Account Number: " << accountNo << endl;
+    cout << "Balance: " << balance << endl;
+
+    return 0;
+}
+
+
+    // Q111. Create Ticket Booking System
+
+    string name;
+    int seats;
+
+    cout << "Enter Customer Name: ";
+    cin >> name;
+
+    cout << "Enter Number of Seats: ";
+    cin >> seats;
+
+    cout << "\nTicket Booked Successfully\n";
+    cout << "Customer Name: " << name << endl;
+    cout << "Seats Booked: " << seats << endl;
+
+    return 0;
+}
+
+
+
+    // Q112. Create Contact Management System
+
+    string name;
+    string phone;
+
+    cout << "Enter Name: ";
+    cin >> name;
+
+    cout << "Enter Phone Number: ";
+    cin >> phone;
+
+    cout << "\nContact Details\n";
+    cout << "Name: " << name << endl;
+    cout << "Phone Number: " << phone << endl;
+
+    return 0;
+}
